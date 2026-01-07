a4bd850fb878913bab7015e4921169cb1b85bd13
Alessandro Lanfrancotti <45257825+alessandrolanfrancotti@users.noreply.github.com>
2026-01-07 21:47:33 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-document.body.style.background = "red";
-alert("JS caricato");
+// Aspettiamo che la scena sia caricata
+document.querySelector('a-scene').addEventListener('click', function () {
+    // Quando clicchi sulla schermata, il mouse viene "catturato" per ruotare la visuale
+    this.canvas.requestPointerLock();
+});
+
+console.log("Sistema di movimento pronto. Usa WASD per muoverti.");
